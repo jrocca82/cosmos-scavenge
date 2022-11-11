e90016fcdde40c21eb2a01085f23f864fece2442
Jo <loluvulol@gmail.com>
2022-11-11 11:46:06 +1000

    Create messages

--- ts-client/scavenge.scavenge/registry.ts
+++ ts-client/scavenge.scavenge/registry.ts
@@ -1,6 +1,12 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCommitSolution } from "./types/scavenge/scavenge/tx";
+import { MsgSubmitScavenge } from "./types/scavenge/scavenge/tx";
+import { MsgRevealSolution } from "./types/scavenge/scavenge/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
+    ["/scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
+    ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
     
 ];
 
